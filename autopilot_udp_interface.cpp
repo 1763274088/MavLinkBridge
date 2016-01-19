b805b38901ed7e05c72ce430abaab04493631d7e
Mohamed Abdelkader Zahana <mohamedashraf123@gmail.com>
2016-01-19 13:00:50 +0300

    debug: udp send

--- autopilot_udp_interface.cpp
+++ autopilot_udp_interface.cpp
@@ -573,7 +573,7 @@ write_mavlink_message(mavlink_message_t &message)
     // Write buffer to UDP socket
     //_write_port(buf,len);
     cout << "dest Address: " <<destAddr<<endl;
-    udp_socket->sendTo(buf, (int)len, destAddr, destPort);
+    udp_socket->sendTo(buf, len, destAddr, destPort);
     
     return len;
 }
