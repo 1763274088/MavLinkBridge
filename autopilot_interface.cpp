472d4b5064fe57c1df2ff7b64a0e28179f0e9198
Mohamed Abdelkader Zahana <mohamedashraf123@gmail.com>
2016-05-22 14:39:11 +0300

    fix delays

--- autopilot_interface.cpp
+++ autopilot_interface.cpp
@@ -1027,7 +1027,7 @@ read_thread()
 	{
 		read_messages();
         //read_messages_raw();
-		//usleep(0.001*1000000); // Read batches at 1KHz?
+		usleep(0.001*1000000); // Read batches at 1KHz?
 	}
 
 	reading_status = false;
@@ -1068,7 +1068,7 @@ write_thread(void)
 	// otherwise it will go into fail safe
 	while ( not time_to_exit )
 	{
-		//usleep(0.001*1000000);   // write at 1KHz?
+		usleep(0.001*1000000);   // write at 1KHz?
 		//write_setpoint();
         
         //write specific commands
--- autopilot_udp_interface.cpp
+++ autopilot_udp_interface.cpp
@@ -1507,7 +1507,7 @@ read_thread()
         read_messages();
         //read_mavlink_messages_2();
         //read_messages_raw();
-        //usleep(0.001*1000000); // Read batches at 1KHz
+        usleep(0.001*1000000); // Read batches at 1KHz
     }
     
     reading_status = false;
@@ -1552,7 +1552,7 @@ write_thread(void)
     // otherwise it will go into fail safe
     while ( not time_to_exit )
     {
-        //usleep(0.001*1000000);   // Stream at 1KHz
+        usleep(0.001*1000000);   // Stream at 1KHz
         //write_setpoint();
         write_to_GC();
         
